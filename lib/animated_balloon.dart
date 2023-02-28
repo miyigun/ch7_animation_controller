19136ba81360abca53ab00b34b9e6622add42843
Murat Iyigun <miyigun@hotmail.com>
2023-02-28 23:41:46 +0300

    Initial commit

---
+++ lib/animated_balloon.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class AnimatedBalloonWidget extends StatefulWidget {
+  const AnimatedBalloonWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedBalloonWidget> createState() => _AnimatedBalloonWidgetState();
+}
+
+class _AnimatedBalloonWidgetState extends State<AnimatedBalloonWidget> with TickerProviderStateMixin {
+  late AnimationController _controllerFloatUp;
+  late AnimationController _controllerGrowSize;
+  late Animation _animationFloatUp;
+  late Animation<double> _animationGrowSize;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerFloatUp =
+        AnimationController(duration: const Duration(seconds: 4), vsync: this);
+    _controllerGrowSize =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controllerFloatUp.dispose();
+    _controllerGrowSize.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _balloonHeight = MediaQuery.of(context).size.height / 2;
+    double _balloonWidth = MediaQuery.of(context).size.height / 3;
+    double _balloonBottomLocation =
+        MediaQuery.of(context).size.height - _balloonHeight;
+    _animationFloatUp=Tween(begin: _balloonBottomLocation, end: 0.0).animate(CurvedAnimation(parent: _controllerFloatUp,curve: Curves.fastOutSlowIn));
+    _animationGrowSize=Tween(begin: 100.0, end: _balloonWidth).animate(CurvedAnimation(parent: _controllerGrowSize,curve: Curves.elasticInOut));
+
+    return AnimatedBuilder(
+        animation: _animationFloatUp,
+        builder: (context, child){
+          return Container(
+            margin: EdgeInsets.only(
+              top: _animationFloatUp.value,
+            ),
+            width: _animationGrowSize.value,
+            child: child,
+          );
+        },
+      child: GestureDetector(
+        onTap: (){
+          if(_controllerFloatUp.isCompleted){
+            _controllerFloatUp.reverse();
+            _controllerGrowSize.reverse();
+          }
+          else {
+            _controllerFloatUp.forward();
+            _controllerGrowSize.forward();
+          }
+        },
+        child: Image.asset(
+          'assets/images/balloon.jpg',
+          height: _balloonHeight,
+          width: _balloonWidth,
+        ),
+      ),
+    );
+  }
+}
